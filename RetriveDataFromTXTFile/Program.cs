93fdb3069f5373c241f671e46ac003c91dae7ecd
Deekshitha <deekshithachatla@gmail.com>
2023-10-23 16:45:18 +0530

    changes have dome

--- RetriveDataFromTXTFile/Program.cs
+++ RetriveDataFromTXTFile/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.NetworkInformation;
 
-namespace Studentdatafile
+namespace RetriveDataFromTXTFile
 {
     internal class Program
     {
